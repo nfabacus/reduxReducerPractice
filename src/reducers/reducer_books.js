bc193f0c9f8dd92a2724829063c8667438c52620
noby <n.fujioka@abacusmaths.info>
2016-09-01 09:09:58 +0100

    completes a minimum flow of reducer, container, action

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'BFG' },
-    { title: 'Hobbit' }
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 200 },
+    { title: 'BFG', pages: 150 },
+    { title: 'Hobbit', pages: 250 }
   ]
 }
